58ad0f27470596597fe7883d96f1ef787a1a4724
unknown <zhangyuanzhu@corp.qihoo.net>
2019-09-12 15:13:47 +0800

    button components complete

---
+++ src/components/button-group/button-group.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="[`${prefixCls}`, vertical && `${prefixCls}-vertical`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-button-group';
+
+export default {
+    name: 'ButtonGroup',
+    props: {
+        vertical: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            prefixCls
+        }
+    },
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import '../../styles/components/_button-group.scss';
+</style>
