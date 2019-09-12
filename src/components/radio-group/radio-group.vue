016f36beabf3fee9db82ca01daf9ea9de21c59a2
unknown <zhangyuanzhu@corp.qihoo.net>
2019-09-12 17:02:09 +0800

    radio radio-group component

---
+++ src/components/radio-group/radio-group.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="[`${prefixCls}`, vertical && `${prefixCls}-vertical`]">
+        <!-- <Radio v-for="(radio, index) in radioData" 
+            :key="radio.value || index" 
+            :active="radio.active" 
+            :value="radio.value"
+            :group="radioGroup"
+             /> -->
+        <slot></slot>     
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-radio-group';
+import Radio from '../radio/index'
+
+export default {
+    name: 'RadioGroup',
+    components: {
+        Radio
+    },
+    props: {
+        radioData: {
+            type: Array,
+            default: () => []
+        },
+        vertical: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            prefixCls,
+            radioGroup: true
+        }
+    },
+
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import '../../styles/components/_radio-group.scss';
+</style>
---
+++ src/components/radio/radio.vue
@@ -0,0 +1,64 @@
+<template>
+    <label :class="[`${prefixCls}`, active && `${prefixCls}-active`, disabled && `${prefixCls}-disabled`]">
+        <span :class="[`${prefixCls}-icon`]"></span>
+        <span :class="`${prefixCls}-value`" v-if="value" v-text="value"></span>
+    </label>
+</template>
+<script>
+
+//是否选中的逻辑在引入组件处处理，所以disabled等选项和多个单选框的时候，需要判断逻辑
+
+
+const prefixCls = "qui-radio";
+
+export default {
+    name: 'Radio',
+    props: {
+        value: [String, Number, Boolean],
+        active: {
+            type: Boolean,
+            default: false
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        group: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            prefixCls,
+            isActive: false,
+            radioGroupValue: null
+        }
+    },
+    methods: {
+        handleClick () {
+            //后续添加处理
+            let { value, isActive, disabled, group, radioGroupValue } = this;
+            if (disabled) return;
+            if (group) {
+                this.radioGroupValue = value;
+            } else {
+                this.isActive = !isActive;
+                if (isActive) {
+                    this.$emit('on-change', '')
+                } else {
+                    this.$emit('on-change', value)
+                }
+            }
+        }
+    },
+    mounted() {
+        // const { active } = this;
+        // this.isActive = active;
+    },
+}
+</script>
+
+<style lang="scss" type="text/scss">
+    @import '../../styles/components/_radio.scss';
+</style>
