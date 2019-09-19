01745c40c37cb6c992184bbf4c3c2762566da966
unknown <zhangyuanzhu@corp.qihoo.net>
2019-09-19 16:57:47 +0800

    slelect component

---
+++ src/components/select/select.vue
@@ -0,0 +1,81 @@
+<template>
+    <div :class="[`${prefixCls}`, `${prefixCls}-${size}`, disabled && `${prefixCls}-disabled`, !arrowDown && `${prefixCls}-active`]">
+        <div :class="[`${prefixCls}-main`]" @click="!disabled && (arrowDown = !arrowDown)">
+            <span :class="`${prefixCls}-value`">{{ selectValue }}</span>
+            <span :class="[arrowDown ? `${prefixCls}-down` : `${prefixCls}-up`, `${prefixCls}-arrow`]"></span>
+        </div>
+        <drop-down :data="dropData" :size="size" :show="!disabled && !arrowDown" :autoWidth="autoWidth" @on-choose="onChoose" />
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-select';
+import DropDown from '../dropdown'
+import { hasParam } from '../../utils/util'
+
+
+export default {
+    name: 'Select',
+    components: {
+        DropDown
+    },
+    props: {
+        data: {
+            type: Array,
+            default: () => []
+        },
+        show: {
+            type: Boolean,
+            default: false
+        },
+        autoWidth: {
+            type: Boolean,
+            default: false
+        },
+        defaultValue: {
+            type: [String, Number],
+            default: '请选择'
+        },
+        size: {
+            type: String,
+            default: 'middle',
+            validator(value) {
+                const valueList = ['small', 'middle', 'large'];
+                return hasParam(value, valueList);
+            }
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            prefixCls,
+            dropData: this.data,
+            selectValue: this.defaultValue,
+            arrowDown: true
+        }
+    },
+    methods: {
+        onChoose (item) {
+            this.dropData.map(i => i.active = false);
+            item.active = !item.active;
+            this.selectValue = item.value;
+            this.arrowDown = !this.arrowDown;
+            this.$emit('on-select', item)
+        },
+        getDefaultValue () {
+            this.dropData.map(i => {
+                if (i.active) this.selectValue = i.value;
+            });
+        },
+    },
+    mounted() {
+        this.getDefaultValue();
+    },
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import "../../styles/components/_select.scss";
+</style>
