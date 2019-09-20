5be6eca1e1ca9d1d4c74432418c4ae856744ea1a
unknown <zhangyuanzhu@corp.qihoo.net>
2019-09-20 16:43:50 +0800

    alert component

---
+++ src/components/alert/alert.vue
@@ -0,0 +1,46 @@
+<template>
+    <transition :name="transition">
+            <div :class="[`${prefixCls}`, `${prefixCls}-${type}`]">
+            alert
+
+        </div>
+    </transition>
+</template>
+
+<script>
+
+const prefixCls = 'qui-alert';
+import { hasParam } from '../../utils/util';
+
+
+export default {
+    name: 'Alert',
+    props: {
+        message: String,
+        duration: {
+            type: Number,
+            default: 3
+        },
+        type: {
+            type: String,
+            default: 'warning',
+            validator (value) {
+                const valueList = ['success', 'warning', 'error', 'info'];
+                return hasParam(value, valueList); 
+            }
+        },
+        transition: {
+            type: String,
+            default: 'alert-fade-in-move-up'
+        }
+    },
+    data() {
+        return {
+            prefixCls
+        }
+    },    
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import "../../styles/components/_alert.scss";
+</style>
