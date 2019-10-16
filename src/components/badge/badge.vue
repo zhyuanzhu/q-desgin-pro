b465bb6eed73cca6e7070d850f74a64feb7e2453
zhangyuanzhu <zhangyuanzhu@corp.qihoo.net>
2019-10-16 16:51:05 +0800

    badge component init

---
+++ src/components/badge/badge.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="[prefixCls]">
+        TODO:Badge
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-badge';
+
+export default {
+    name: 'Badge',
+    props: {
+
+    },
+    data () {
+        return {
+            prefixCls
+        }
+    }
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import "../../styles/components/_badge.scss";
+</style>
