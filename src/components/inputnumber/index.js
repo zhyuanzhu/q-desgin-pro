8338507619116ef9349a2be4e3a37dccfc414c7a
unknown <zhangyuanzhu@corp.qihoo.net>
2019-10-16 16:45:54 +0800

    inputNumber component init

---
+++ src/components/inputnumber/index.js
@@ -0,0 +1,3 @@
+import InputNumber from './inputnumber.vue';
+
+export default InputNumber;
---
+++ src/components/inputnumber/inputnumber.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="[prefixCls]">
+        TODO:InputNumber
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-inputNumber';
+
+export default {
+    name: 'InputNumber',
+    props: {
+
+    },
+    data () {
+        return {
+            prefixCls
+        }
+    }
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import "../../styles/components/_inputnumber.scss";
+</style>
