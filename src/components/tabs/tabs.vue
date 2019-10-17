40bcecd851f773808f755b5d81a72032c9a9ea93
zhangyuanzhu <zhangyuanzhu@corp.qihoo.net>
2019-10-17 16:14:55 +0800

    init tabs component

---
+++ src/components/tabs/tabs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="[prefixCls]">
+        TODO:Tabs
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-tabs';
+
+export default {
+    name: 'Tabs',
+    props: {
+
+    },
+    data () {
+        return {
+            prefixCls
+        }
+    }
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import "../../styles/components/_tabs.scss";
+</style>
