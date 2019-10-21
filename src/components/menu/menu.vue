844540a5fad975e74c11c525f459f93148e25808
zhangyuanzhu <zhangyuanzhu@corp.qihoo.net>
2019-10-21 16:19:25 +0800

    init menu component

---
+++ src/components/menu/menu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        TODO:Menu
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-menu';
+
+export default {
+    name: 'Menu',
+    props: {
+
+    },
+    data () {
+        return {
+            prefixCls
+        }
+    }
+}
+</script>
+<style lang="scss" type="text/scss">
+    @import "../../styles/components/_menu.scss";
+</style>
