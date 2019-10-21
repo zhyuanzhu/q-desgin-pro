4d57b6bbed2de7eb45646f5d1adbb47e37e58428
zhangyuanzhu <zhangyuanzhu@corp.qihoo.net>
2019-10-21 14:18:21 +0800

    badge component version 1

--- src/components/badge/badge.vue
+++ src/components/badge/badge.vue
@@ -1,22 +1,39 @@
 <template>
     <div :class="[prefixCls]">
-        TODO:Badge
+        <span v-if="count > 0">{{ setCount(count, maxCount) }}</span>
     </div>
 </template>
 <script>
 
-const prefixCls = 'qui-badge';
+const prefixCls = 'qui-badge'; 
 
 export default {
     name: 'Badge',
     props: {
-
+        count: {
+            type: Number,
+            default: 0
+        },
+        maxCount: Number,
+        // status: {
+        //     type: String,
+        //     default: 'success'
+        // },
+        
     },
     data () {
         return {
             prefixCls
         }
-    }
+    },
+    methods: {
+        setCount (count, maxCount = 0) {
+            if (count > maxCount) {
+                return `${maxCount}+`;
+            }
+            return count;
+        }
+    },
 }
 </script>
 <style lang="scss" type="text/scss">
