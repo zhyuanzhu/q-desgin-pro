c2793ae86ffd1cce414167c7654fdd0f5bc2987e
zhangyuanzhu <zhangyuanzhu@corp.qihoo.net>
2019-10-23 18:17:13 +0800

    npm publish config.js

---
+++ build/build-style.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+
+// 编译less
+gulp.task('css', function () {
+    gulp.src('../src/styles/index.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie > 8']
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename('qdesign.css'))
+        .pipe(gulp.dest('../dist/styles'));
+});
+
+// 拷贝字体文件
+// gulp.task('fonts', function () {
+//     gulp.src('../src/styles/common/iconfont/fonts/*.*')
+//         .pipe(gulp.dest('../dist/styles/fonts'));
+// });
+
+// gulp.task('default', ['css', 'fonts']);
+gulp.task('default', ['css']);
---
+++ build/package.prod.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./package.config.js');
+const CompressionPlugin = require('compression-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+process.env.NODE_ENV = 'production';
+
+module.exports = merge(webpackBaseConfig, {
+    devtool: 'source-map',
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/',
+        filename: 'qdesgin.min.js',
+        library: 'qdesgin',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    externals: {
+        vue: {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+        }
+    },
+    plugins: [
+        // @todo
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        }),
+        new UglifyJsPlugin({
+            parallel: true,
+            sourceMap: true
+        }),
+        new CompressionPlugin({
+            assert: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.(js|css)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ]
+});
