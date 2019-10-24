564cd2771545374417e4ef91848331ef05305309
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-10-24 11:51:20 +0800

    dist entry js

--- build/package.dev.config.js
+++ build/package.dev.config.js
@@ -1,25 +1,38 @@
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./package.config')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const extractScss = new ExtractTextPlugin('/ydui.min.css')
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./package.config.js');
 
-module.exports = merge(baseWebpackConfig, {
-  output: {
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [{
-      test: /\.scss$/i,
-      loader: extractScss.extract(['css-loader','sass-loader'])
-    }]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"development"'
-      }
-    }),
-    extractScss
-  ]
-})
+process.env.NODE_ENV = 'production';
+
+module.exports = merge(webpackBaseConfig, {
+    devtool: 'source-map',
+
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/',
+        filename: 'qdesgin.js',
+        library: 'qdesgin',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    externals: {
+        vue: {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+        }
+    },
+    plugins: [
+        // @todo
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ]
+});
