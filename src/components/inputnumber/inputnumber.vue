945be6d5645969d099896b7630f6470bba348b19
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-10-28 17:21:37 +0800

    input-number fixed

--- src/components/inputnumber/inputnumber.vue
+++ src/components/inputnumber/inputnumber.vue
@@ -1,22 +1,55 @@
 <template>
     <div :class="[prefixCls]">
-        TODO:InputNumber
+        <div :class="`${prefixCls}-main`">
+            <!-- oninput = "value=value.replace(/[^\d\.{0,1}]/g,'')" -->
+            <input type="text" 
+                :class="`${prefixCls}-${size} input-number`" 
+                autocomplete="off"
+                
+                >
+            <span :class="`${prefixCls}-steps`">
+                <i :class="`${prefixCls}-up arrow`"></i>
+                <i :class="`${prefixCls}-down arrow`"></i>
+            </span>
+            <span v-if="unit.length">{{ unit }}</span>
+        </div>
     </div>
 </template>
 <script>
+import { hasParam } from '../../utils/util';
 
 const prefixCls = 'qui-inputNumber';
 
 export default {
     name: 'InputNumber',
     props: {
-
+        steps: {
+            type: Number,
+            default: 1
+        },
+        size: {
+            type: String,
+            default: 'middle',
+            validator (value) {
+                const valueList = ['small', 'middle', 'large'];
+                return hasParam(value, valueList);
+            }
+        },
+        unit: {
+            type: String,
+            default: ''
+        }
     },
     data () {
         return {
             prefixCls
         }
-    }
+    },
+    methods: {
+        change () {
+            // value=value.replace(/[^\d]/g,'')
+        }
+    },
 }
 </script>
 <style lang="scss" type="text/scss">
