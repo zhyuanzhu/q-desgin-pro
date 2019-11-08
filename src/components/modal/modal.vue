8be4a3039f40b059a9ce5315ece000d6fac6b89f
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-11-08 16:42:33 +0800

    添加动画参数，避免遮罩层出现动画

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -1,21 +1,23 @@
 <template>
-    <div :class="[`${prefixCls}`]" v-show="isShow">
-        <div :class="`${prefixCls}-main`">
-            <section :class="`${prefixCls}-header`" v-if="header">
-                <h3 :class="`${prefixCls}-header-title`" v-text="title"></h3>
-                <span :class="`${prefixCls}-header-close`" v-if="close" @click="cancel"></span>
-            </section>
-            <section :class="`${prefixCls}-container`"><slot></slot></section>
-            <section :class="`${prefixCls}-footer`" v-if="footer">
-                <div :class="`${prefixCls}-footer-btn`">
-                    <button-group>
-                        <Button v-if="hasConfirm" :type="confitmType" :size="'large'" v-text="confirmTxt" @click.native="confirm"></Button>
-                        <Button v-if="hasCancel" :type="cancelType" :size="'large'" v-text="cancelTxt" @click.native="cancel"></Button>
-                    </button-group>
-                </div>
-            </section>
-        </div>
-        <div :class="`${prefixCls}-mask`" v-if="mask" @click="closeMask"></div>
+    <div :class="[`${prefixCls}`]">
+        <transition :name="animate">
+            <div :class="`${prefixCls}-main`" v-if="value">
+                <section :class="`${prefixCls}-header`" v-if="header">
+                    <h3 :class="`${prefixCls}-header-title`" v-text="title"></h3>
+                    <span :class="`${prefixCls}-header-close`" v-if="close" @click="cancel"></span>
+                </section>
+                <section :class="`${prefixCls}-container`"><slot></slot></section>
+                <section :class="`${prefixCls}-footer`" v-if="footer">
+                    <div :class="`${prefixCls}-footer-btn`">
+                        <button-group>
+                            <Button v-if="hasConfirm" :type="confitmType" :size="'large'" v-text="confirmTxt" @click.native="confirm"></Button>
+                            <Button v-if="hasCancel" :type="cancelType" :size="'large'" v-text="cancelTxt" @click.native="cancel"></Button>
+                        </button-group>
+                    </div>
+                </section>
+            </div>
+        </transition>
+        <div :class="`${prefixCls}-mask`" v-if="mask" @click="closeMask" v-show="value"></div>
     </div>
 </template>
 
@@ -76,13 +78,17 @@ export default {
             type: Boolean,
             default: false
         },
-        title: String
+        title: String,
+        animate: {
+            type: String,
+            default: 'slide-down'
+        }
 
     },
     data() {
         return {
             prefixCls,
-            isShow: this.value
+            // isShow: this.value
         }
     },
     methods: {
@@ -94,19 +100,27 @@ export default {
             this.$emit('on-cancel', evt)
         },
         hide () {
-            this.isShow = false;
+            this.value = false;
         },
         closeMask () {
-            this.hide();
+            // this.hide();
+            this.cancel()
         }
     },
-    watch: {
-        value (show) {
-            this.isShow = show;
-        }
-    },
+    // watch: {
+    //     value (show) {
+    //         this.isShow = show;
+    //     }
+    // },
 }
 </script>
 <style lang="scss" type="text/scss">
     @import "../../styles/components/_modal.scss";
+    // .slide-down-enter, .slide-down-leave-to {
+    //     opacity: 0;
+    //     transform: translateY(-400px);
+    // }
+    // .slide-down-enter-active, .slide-down-leave-active {
+    //     transition: all .8s;
+    // }
 </style>
