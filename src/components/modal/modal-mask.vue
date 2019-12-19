d5d6ac2553616996e42aad353a13333e11166467
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-12-19 17:52:50 +0800

    add modal-mask component, update modal component

---
+++ src/components/modal/modal-mask.vue
@@ -0,0 +1,33 @@
+<template>
+    <transition name="fade-modal-mask">
+        <div :class="prefix" v-if="visible" @click="handleClick" :style="{'z-index': zIndex}"></div>
+    </transition>
+</template>
+<script>
+
+const prefix = 'qui-modal-mask';
+
+export default {
+    name: 'ModalMask',
+    props: {
+        zIndex: {
+            type: Number,
+            required: true
+        },
+        visible: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data() {
+        return {
+            prefix
+        }
+    },
+    methods: {
+        handleClick () {
+            this.$emit('on-hide-mask')
+        }
+    },
+}
+</script>
--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -1,7 +1,7 @@
 <template>
-    <div :class="[`${prefixCls}`]">
-        <transition :name="animate">
-            <div :class="`${prefixCls}-main`" v-if="value">
+    <div :class="`${prefixCls}`" :style="styles">
+        <transition :name="animate">  
+            <div :class="`${prefixCls}-main`" :style="mainstyles" v-if="visible" ref="modal">
                 <section :class="`${prefixCls}-header`" v-if="header">
                     <h3 :class="`${prefixCls}-header-title`" v-text="title"></h3>
                     <Icon :type="'close'" :size="24" :color="'#999'" :class="`${prefixCls}-header-close`" v-if="close" @click.native="cancel" />
@@ -10,14 +10,14 @@
                 <section :class="`${prefixCls}-footer`" v-if="footer">
                     <div :class="`${prefixCls}-footer-btn`">
                         <button-group>
-                            <Button v-if="hasConfirm" :type="confitmType" :size="'large'" v-text="confirmTxt" @click.native="confirm"></Button>
-                            <Button v-if="hasCancel" :type="cancelType" :size="'large'" v-text="cancelTxt" @click.native="cancel"></Button>
+                            <Button v-if="hasConfirm"  :size="'large'" v-text="confirmTxt" @click.native="confirm"></Button>
+                            <Button v-if="hasCancel" :type="'primary'" :size="'large'" v-text="cancelTxt" @click.native="cancel"></Button>
                         </button-group>
                     </div>
                 </section>
             </div>
         </transition>
-        <div :class="`${prefixCls}-mask`" v-if="mask" @click="closeMask" v-show="value"></div>
+        <modal-mask :visible="visible" :zIndex="zIndex - 1" @on-hide-mask="hideMask" />
     </div>
 </template>
 
@@ -26,11 +26,13 @@
 import ButtonGroup from '../button-group'
 import Button from '../button'
 import Icon from '../icon'
+import ModalMask from './modal-mask'
 
 const prefixCls = 'qui-modal';
 export default {
     name: 'Modal',
     components: {
+        ModalMask,
         ButtonGroup,
         Button,
         Icon
@@ -56,14 +58,6 @@ export default {
             type: String,
             default: '取消'
         },
-        confitmType: {
-            type: String,
-            default: 'main'
-        },
-        cancelType: {
-            type: String,
-            default: 'primary'
-        },
         hasConfirm: {
             type: Boolean,
             default: true
@@ -76,21 +70,25 @@ export default {
             type: Boolean,
             default: true
         },
-        value: {
+        visible: {
             type: Boolean,
             default: false
         },
         title: String,
         animate: {
             type: String,
-            default: 'slide-down'
+            default: 'modal-slide-down'
+        },
+        width: {
+            type: Number,
+            default: 500
         }
-
     },
     data() {
         return {
             prefixCls,
-            // isShow: this.value
+            zIndex: 9999,
+            height: 0
         }
     },
     methods: {
@@ -100,28 +98,29 @@ export default {
         cancel (evt) {
             this.$emit('on-cancel', evt)
         },
-        hide () {
-            this.value = false;
-        },
-        closeMask () {
-            // this.hide();
+        hideMask () {
             this.cancel()
         }
     },
-    // watch: {
-    //     value (show) {
-    //         this.isShow = show;
-    //     }
-    // },
+    computed: {
+        styles () {
+            const { width, height } = this;
+            return `margin-left: -${ width/2 }px`;
+        },
+        mainstyles () {
+            const { zIndex, width } = this;
+            return `z-index: ${zIndex}; width: ${width}px`;
+        }
+    },
+    watch: {
+        visible (value) {
+            if (this.visible) {
+                this.zIndex = this.zIndex + 1
+            } 
+        }
+    },
 }
 </script>
 <style lang="scss" type="text/scss">
     @import "../../styles/components/_modal.scss";
-    // .slide-down-enter, .slide-down-leave-to {
-    //     opacity: 0;
-    //     transform: translateY(-400px);
-    // }
-    // .slide-down-enter-active, .slide-down-leave-active {
-    //     transition: all .8s;
-    // }
 </style>
