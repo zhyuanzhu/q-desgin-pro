2e62e03fec5484262916505f0354461566ba4de4
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-12-19 16:12:58 +0800

    update menu component

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -1,26 +1,25 @@
 <template>
     <div :class="[prefixCls, `${prefixCls}-${theme}`]">
         <ul :class="[`${prefixCls}-list`]" :style="{'width': width}">
-            <li v-for="(menu, index) in data" :class="[`${prefixCls}-item`]" :key="menu.id || index">
-                <div :class="[`${prefixCls}-item-title`, !hasChild(menu) && menu.active && 'item-title-active', color && `green-list`]" @click="toggleMenu(data, menu, menu.active)">
+            <li v-for="(menu, index) in data" 
+            :class="[`${prefixCls}-item`]" 
+            :key="menu.id || index">
+                <div :class="[`${prefixCls}-item-title`, !hasChild(menu) && menu.active && 'item-title-active']" 
+                    @click="toggleMenu(data, menu, menu.active)"
+                    >
                     <i :class="[iconClass, menu.icon && `${prefixCls}-${menu.icon}`]"></i>
                     <a href="javascript:;">{{ menu.label }}</a>
-                    <!-- <component :is="tagName(menu)" v-bind="tagProps(menu)" >{{ menu.label }}</component> -->
-                    <i v-if="hasChild(menu)" 
-                        :class="[hasChild(menu) && `${prefixCls}-item-sub`, 
-                        menu.active ? `${prefixCls}-item-sub-up`: `${prefixCls}-item-sub-down`]">
-                        <svg class="svg-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M472.064 751.552 72.832 352.32c-22.08-22.08-22.08-57.792 0-79.872 22.016-22.016 57.792-22.08 79.872 0L512 631.744l359.296-359.296c22.016-22.016 57.792-22.08 79.872 0 22.08 22.08 22.016 57.792 0 79.872l-399.232 399.232C529.856 773.568 494.144 773.568 472.064 751.552z" p-id="1833"></path>
-                        </svg>    
-                        
-                    </i>
+                    <Icon v-if="hasChild(menu)" 
+                        :type="'arrow-up'" 
+                        :size="16" 
+                        :class="[`${prefixCls}-arrow`, menu.active ? `${prefixCls}-arrow-up`: `${prefixCls}-arrow-down`]" />
                 </div>
                 <transition :name="setTransition(menu)">
                     <div :class="[`${prefixCls}-item-children`, `${menu.id}-children`]" v-if="hasChild(menu)" v-show="menu.active">
                         <ul :class="`${menu.id}-list`">
                             <li v-for="(item, idx) in menu.children" :key="item.id || idx" 
                                 @click="clickItem(menu.children, item, item.active, data)"
-                                :class="[`item`, `${item.id}-item`, item.active && `item-active`, color && `green-item`]"
+                                :class="[`item`, `${item.id}-item`, item.active && `item-active`]"
                                 >
                                 <!-- <router-link :to="item.to">{{ item.label }}</router-link> -->
                                 <a href="javascript:;">{{ item.label }}</a>
@@ -33,13 +32,15 @@
     </div>
 </template>
 <script>
-import { hasParam } from '../../utils/util';
 
 const [prefixCls, iconClass] = ['qui-menu', 'menu-icon'];
+import { hasParam } from '../../utils/util';
+import Icon from '../icon'
 //跳转的version-1，需要写跳转的绝对路径 /xx的形式，后续会添加判断，类似于button
 
 export default {
     name: 'Menu',
+    components: { Icon },
     props: {
         theme: {
             type: String,
@@ -58,10 +59,6 @@ export default {
         width: {
             type: String,
             default: '240px'
-        },
-        color: {
-            type: Boolean,
-            default: false
         }
     },
     data () {
