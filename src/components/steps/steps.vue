0224d17db45b7a7901b2042fcb2a8315c5d596a5
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-12-20 11:37:29 +0800

    use Icon component replace status img, fix column line styles;add item icon choice

--- src/components/steps/steps.vue
+++ src/components/steps/steps.vue
@@ -3,26 +3,45 @@
         <div v-for="(step, index) in data" :key="step.id || index" 
         :class="[`${prefixCls}-item`, index + 1 == current && `${prefixCls}-active`, index + 1 < error && `${prefixCls}-err`,
         index + 1 < current  && `${prefixCls}-finished`,  index + 1 == error && `${prefixCls}-error`]">
-            <div :class="`${prefixCls}-icon`">
-                <span v-if="error == index + 1" class="error-icon">+</span>
-                <span v-else-if="index + 1 < current" class="step-icon active"></span>
+            <div :class="`${prefixCls}-icon`" v-if="!step.icon">
+                <span v-if="error == index + 1" class="error-icon">
+                    <Icon :type="'wrong'" :size="18" />
+                </span>
+                <span v-else-if="index + 1 < current" class="step-icon active">
+                    <Icon :type="'check'" :size="18" />
+                </span>
                 <span v-else class="step-icon" :class="error == index + 1 && 'active'"> {{ index + 1 }} </span>
             </div>
-            <div :class="`${prefixCls}-content`">
-                <p class="title">{{ step.title }}</p>
-                <p class="summary" v-if="step.content" v-text="step.content"></p>
+            <div :class="`${prefixCls}-icon no-border`" v-else>
+                <span :class="setIconWrap(index)">
+                    <Icon :type="step.icon" :size="18" />
+                </span>
             </div>
-            <p class="line" v-if="index + 1 < data.length"></p>
+            <div :class="`${prefixCls}-content`">
+                <p class="title"><slot name="title">{{ step.title }}</slot></p>
+                <p class="summary" v-if="step.content || $slots.summary">
+                    <slot name="summary">{{ step.content }}</slot>
+                </p>
+            </div>
+            <p class="line" :class="step.icon && 'no-border-line'" v-if="index + 1 < data.length"></p>
         </div>
+        <!-- <slot></slot> -->
     </div>
 </template>
 <script>
-import { hasParam } from '../../utils/util';
+
+//后续考虑分离Steps和Step
 
 const prefixCls = 'qui-steps';
+import { hasParam } from '../../utils/util';
+import Icon from '../icon'
+
 
 export default {
     name: 'Steps',
+    components: {
+        Icon
+    },
     props: {
         data: {
             type: Array,
@@ -45,6 +64,14 @@ export default {
     data() {
         return {
             prefixCls
+        }
+    },
+    methods: {
+        setIconWrap (index) {
+            const { current, error } = this;
+            if (error == index + 1 ) return 'error-icon noboder'
+            if (index + 1 < current) return 'step-icon active noboder'
+            // if (error == index + 1)
         }
     },
 }
