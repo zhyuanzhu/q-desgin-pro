891d15101295066e42844bd88edbcb6d014f5341
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-12-20 15:44:07 +0800

    date-picker mixins

---
+++ src/components/date-picker/mixins.js
@@ -0,0 +1,20 @@
+export default {
+    props: {
+        disabledHours: {
+            type: Array,
+            default: () => []
+        },
+        disabledMinutes: {
+            type: Array,
+            default: () => []
+        },
+        disabledSeconds: {
+            type: Array,
+            default: () => []
+        },
+        hideDisabledOptions: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
