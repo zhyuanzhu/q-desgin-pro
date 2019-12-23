beffbc1f347dadda23ae8baf598705bc7b72bfe3
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-12-23 15:00:52 +0800

    add some util fn

--- src/utils/util.js
+++ src/utils/util.js
@@ -47,7 +47,104 @@ export function findComponentChildren (context, componentName, ignoreComponentNa
     }, [])
 }
 
+/**
+ * deepcopy
+ * @param {*} data 
+ */
+export function deepCopy (data) {
+    const t = typeOf(data);
+    let o;
 
+    if (t === 'array') {
+        o = [];
+    } else if ( t === 'object') {
+        o = {};
+    } else {
+        return data;
+    }
+
+    if (t === 'array') {
+        for (let i = 0; i < data.length; i++) {
+            o.push(deepCopy(data[i]));
+        }
+    } else if ( t === 'object') {
+        for (let i in data) {
+            o[i] = deepCopy(data[i]);
+        }
+    }
+    return o;
+}
+
+
+/**
+ * scrollTop animation
+ * @param {*} el 
+ * @param {*} from 
+ * @param {*} to 
+ * @param {*} duration 
+ * @param {*} endCallback 
+ */
+export function scrollTop(el, from = 0, to, duration = 500, endCallback) {
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = (
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback) {
+                return window.setTimeout(callback, 1000/60);
+            }
+        );
+    }
+    const difference = Math.abs(from - to);
+    const step = Math.ceil(difference / duration * 50);
+
+    function scroll(start, end, step) {
+        if (start === end) {
+            endCallback && endCallback();
+            return;
+        }
+
+        let d = (start + step > end) ? end : start + step;
+        if (start > end) {
+            d = (start - step < end) ? end : start - step;
+        }
+
+        if (el === window) {
+            window.scrollTo(d, d);
+        } else {
+            el.scrollTop = d;
+        }
+        window.requestAnimationFrame(() => scroll(d, end, step));
+    }
+    scroll(from, to, step);
+}
+
+/**
+ * 
+ * @param {*} str 
+ */
+export function firstUpperCase(str) {
+    return str.toString()[0].toUpperCase() + str.toString().slice(1);
+}
+
+/**
+ * 
+ * @param {dom} element 
+ * @param {*} styleName 
+ */
+export function getStyle (element, styleName) {
+    if (!element || !styleName) return null;
+    styleName = camelCase(styleName);
+    if (styleName === 'float') {
+        styleName = 'cssFloat';
+    }
+    try {
+        const computed = document.defaultView.getComputedStyle(element, '');
+        return element.style[styleName] || computed ? computed[styleName] : null;
+    } catch(e) {
+        return element.style[styleName];
+    }
+}
 
 function typeOf (obj) {
     const toString  = Object.prototype.toString;
