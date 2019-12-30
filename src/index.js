28bb24eeb9afbe45cf14874a56ea3229aeda4abb
zhangyuanzhu <zhangyuanzhu@360.cn>
2019-12-30 14:51:52 +0800

    export components

--- src/index.js
+++ src/index.js
@@ -1,73 +1,81 @@
+// export component
+import Alert from './components/alert'
+
 import Button from './components/button'
 
 import ButtonGroup from './components/button-group'
 
-import Radio from './components/radio'
+import Checkbox from  './components/checkbox'
 
-import RadioGroup  from './components/radio-group'
+import DatePicker from './components/date-picker'
 
-import CheckBox from './components/checkbox'
+import DropDown  from './components/dropdown'
 
 import Empty from './components/empty'
 
-import Modal from './components/modal'
+import Icon from './components/icon'
 
-import Alert from './components/alert'
-
-import Input from './components/input'
-
-import Select from './components/select'
-
-import DropDown from './components/dropdown'
-
-import TextArea from './components/textarea'
-
-import Pagination from './components/pagination'
+import Input from './components/icon'
 
 import Loading from './components/loading'
 
-import Steps from './components/steps'
-
-import TimeLine from './components/timeline'
-
-import Upload from './components/upload'
-
-import Table from './components/table'
-
-import qSwitch from './components/switch'
+import Menu from './components/menu'
 
 import Message from './components/message'
 
+import Modal from './components/modal'
+
+import Option from './components/option'
+
+import Pagination from './components/pagination'
+
+import Radio from './components/radio'
+
+import RadioGroup from './components/radio-group'
+
+import Select from './components/select'
+
+import Steps from './components/steps'
+
+import qSwitch from './components/switch'
+
+import Table from './components/table'
+
 import Tabs from './components/tabs'
 
-import Badge from './components/badge'
+import Textarea from './components/textarea'
 
-import Menu from './components/menu'
+import Timeline from './components/timeline'
+
+import Upload from './components/upload'
+
 
 const components = {
+    Alert,
     Button,
     ButtonGroup,
+    Checkbox,
+    DatePicker,
+    DropDown,
+    Empty,
+    Icon,
+    Input,
+    Loading,
+    Menu,
+    Message,
+    Modal,
+    Option,
+    Pagination,
     Radio,
     RadioGroup,
-    CheckBox,
-    Empty,
-    Modal,
-    Alert,
-    Input,
     Select,
-    DropDown,
-    TextArea,
-    Pagination,
-    Loading,
     Steps,
-    TimeLine,
-    Upload,
-    Table,
     qSwitch,
-    Message,
+    Table,
     Tabs,
-    Badge,
-    Menu
+    Textarea,
+    Timeline,
+    Upload
 };
 
 const install = function (Vue, options = {}) {
