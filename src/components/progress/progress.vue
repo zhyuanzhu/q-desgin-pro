3b6425b6b241bb139dfd4d17712c33ae574aee54
zhangyuanzhu <zhangyuanzhu@360.cn>
2020-01-03 10:58:19 +0800

    init progress components

---
+++ src/components/progress/progress.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="prefixCls">
+        
+    </div>
+</template>
+<script>
+
+const prefixCls = 'qui-progress';
+import { hasParam } from '../../utils/util';
+import Icon from '../icon'
+
+export default {
+    name: 'Progress',
+    components: { Icon },
+    props: {
+        status: {
+            type: String,
+            default: 'normal',
+            validator (value) {
+                const valueList = ['noraml', 'active', 'wrong', 'success'];
+                return hasParam(value, valueList);
+            }
+        },
+        percent: {
+            type: Number,
+            default: 0
+        }
+    },
+    data() {
+        return {
+            prefixCls,
+            currentStatus: this.status
+        }
+    },
+}
+</script>
