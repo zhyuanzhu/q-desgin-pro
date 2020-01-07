461d355c12cf7d3eb761a4e2671e9d6375a02a28
zhangyuanzhu <zhangyuanzhu@360.cn>
2020-01-07 17:19:53 +0800

    getStyle util

--- src/utils/util.js
+++ src/utils/util.js
@@ -127,6 +127,16 @@ export function firstUpperCase(str) {
     return str.toString()[0].toUpperCase() + str.toString().slice(1);
 }
 
+
+const SPECIAL_CHARS_REGEXP = /([\:\-\_]+(.))/g;
+const MOZ_HACK_REGEXP = /^moz([A-Z])/;
+
+function camelCase(name) {
+    return name.replace(SPECIAL_CHARS_REGEXP, function(_, separator, letter, offset) {
+        return offset ? letter.toUpperCase() : letter;
+    }).replace(MOZ_HACK_REGEXP, 'Moz$1');
+}
+
 /**
  * 
  * @param {dom} element 
