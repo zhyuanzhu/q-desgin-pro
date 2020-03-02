778b28cfd45e58a206325f8a398008b3353b439c
zhangyuanzhu <zhangyuanzhu@360.cn>
2020-03-02 15:44:26 +0800

    row, col

--- src/components/grid/col.vue
+++ src/components/grid/col.vue
@@ -1,10 +1,61 @@
 <template>
-    <div>
-        TODO: Col
+    <div :class="classes" :style="styles">
+        <slot></slot>
     </div>
 </template>
 <script>
+
+const prefixCls = 'qui-col';
+import { findComponentParents } from '../../utils/util'
+
 export default {
-    name: 'Col'
+    name: 'Col',
+    props: {
+        span: [Number, String],
+        order: [Number, String],
+        offset: [Number, String],
+        push: [Number, String],
+        pull: [Number, String],
+        xs: [Number, Object],
+        sm: [Number, Object],
+        md: [Number, Object],
+        lg: [Number, Object],
+        xl: [Number, Object],
+        xxl: [Number, Object]
+        className: String,
+    },
+    data() {
+        return {
+            prefixCls,
+            gutter: 0    
+        }
+    },
+    computed: {
+        classes () {
+
+        },
+        styles () {
+            let style = {};
+            const { gutter } = this;
+            if (gutter !== 0) {
+                const dis = gutter / -2;
+                style = `margin-left: ${dis}px; margin-right: ${dis}px;`
+            }
+            return style;
+        }
+    },
+    mounted() {
+        this.updateGutter()
+    },
+    methods: {
+        updateGutter () {
+            const Row = findComponentParents(this, 'Row');
+            if (!Row) return;
+            Row.updateGutter(Row.gutter);
+        }
+    },
+    beforeDestroy() {
+        this.updateGutter()
+    },
 }
 </script>
--- src/components/grid/row.vue
+++ src/components/grid/row.vue
@@ -1,10 +1,80 @@
 <template>
-    <div>
-        TODO: Row
+    <div :class="clssses" :style="styles">
+        <slot></slot>
     </div>
 </template>
 <script>
+
+const prefixCls = 'qui-row';
+import { hasParam, findComponentChildren } from '../../utils/util';
+
 export default {
-    name: 'Row'
+    name: 'Row',
+    props: {
+        type: {
+            validator (value) {
+                return hasParam(value, ['flex'])
+            }
+        },
+        align: {
+            validator (value) {
+                const valueList = ['top', 'middle', 'bottom'];
+                return hasParam(value, valueList)
+            }
+        },
+        justify: {
+            validator (value) {
+                const valueList = ['start', 'end', 'center', 'space-around', 'space-between'];
+                return hasParam(value, valueList)
+            }
+        },
+        gutter: {
+            type: Number,
+            default: 0
+        },
+        className: String
+    },
+    data() {
+        return {
+            prefixCls
+        }
+    },
+    computed: {
+        clssses () {
+            const { prefixCls, type, align, justify, className } = this;
+            return [{
+                [`${prefixCls}`]: !type,
+                [`${prefixCls}-${type}`]: !!type,
+                [`${prefixCls}-${type}-${align}`]: !!align,
+                [`${prefixCls}-${type}-${justify}`]: !!justify,
+                [`${className}`]: !!className
+            }]
+        },
+        styles () {
+            let style = {};
+            const { gutter } = this;
+            if (gutter !== 0) {
+                const dis = gutter / -2;
+                style = `margin-left: ${dis}px; margin-right: ${dis}px;`
+            }
+            return style;
+        }
+    },
+    watch: {
+        gutter (v) {
+            this.updateGutter(v);
+        }
+    },
+    methods: {
+        updateGutter (val) {
+            const Col = findComponentChildren(this, 'Col');
+            const colList = findComponentBrothers(Col, 'Col', false);
+            colList.length > 0 && colList.forEach(child => {
+                if (v !== 0) {
+                    child.gutter = val;
+                }
+            })
+        }
+    },
 }
 </script>
