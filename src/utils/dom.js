b5a31ceab019edbb738a0284f10fa441b4cf085e
zyz <alphazhangyz@gmail.com>
2020-04-18 21:04:03 +0800

    remove dom class

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -47,3 +47,30 @@ export function addClass(el, cls) {
         el.className = curClass;
     }
 }
+
+/**
+ * 
+ * @param {DOM} el 
+ * @param {String} cls 
+ */
+export function removeClass(el, cls) {
+    if (!el || !cls) return;
+    const classes = cls.split(' ');
+    let curClass = ' ' + el.className + ' ';
+
+    for (let i = 0, j = classes.length; i < j; i++) {
+        const clsName = classes[i];
+        if (!clsName) continue;
+
+        if (el.classList) {
+            el.classList.remove(clsName);
+        } else {
+            if (hasClass(el, clsName)) {
+                curClass = curClass.replace(' ' + clsName + ' ', ' ');
+            }
+        }
+    }
+    if (!el.classList) {
+        el.className = trim(curClass);
+    }
+}
