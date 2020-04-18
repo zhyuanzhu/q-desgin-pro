04dadd9a6fdbdc9275c6ed5355693b756d1a3a04
zyz <alphazhangyz@gmail.com>
2020-04-18 20:57:45 +0800

    set/remove dom className

---
+++ src/utils/dom.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ * @param {String} string 
+ */
+const trim = function(string) {
+    return (string || '').replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, '');
+};
+
+/**
+ * 
+ * @param {DOM} el 
+ * @param {String} cls 
+ */
+export function hasClass(el, cls) {
+    if (!el || !cls) return false;
+    if (cls.indexOf(' ') !== -1) throw new Error('className should not contain space.');
+    if (el.classList) {
+        return el.classList.contains(cls);
+    } else {
+        return (' ' + el.className + ' ').indexOf(' ' + cls + ' ') > -1;
+    }
+}
+
+/**
+ * 
+ * @param {DOM} el 
+ * @param {String} cls 
+ */
+export function addClass(el, cls) {
+    if (!el) return;
+    let curClass = el.className;
+    const classes = (cls || '').split(' ');
+
+    for (let i = 0, j = classes.length; i < j; i++) {
+        const clsName = classes[i];
+        if (!clsName) continue;
+
+        if (el.classList) {
+            el.classList.add(clsName);
+        } else {
+            if (!hasClass(el, clsName)) {
+                curClass += ' ' + clsName;
+            }
+        }
+    }
+    if (!el.classList) {
+        el.className = curClass;
+    }
+}
