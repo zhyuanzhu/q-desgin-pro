3d28983561c96e1491623567d0ab3eba317f7c14
zyz <alphazhangyz@gmail.com>
2020-04-19 01:23:10 +0800

    collapse components

---
+++ src/components/collapse/collapse-item.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="`${prefixCls}-wrap`">
+        <div :class="`${prefixCls}-header`"></div>
+        <collapse-transition>
+            <div :class="`${prefixCls}-content`" v-show="isActive">
+                <slot></slot>
+            </div>
+        </collapse-transition>
+    </div>
+</template>
+
+<script>
+
+const prefixCls = 'qui-collapse'
+import CollapseTransition from '../collapse-transition'
+
+export default {
+    name: 'CollapseItem',
+    components: { CollapseTransition },
+    data() {
+        return {
+            prefixCls,
+            isActive: false
+        }
+    },
+}
+</script>
---
+++ src/components/collapse/collapse.vue
@@ -0,0 +1,25 @@
+
+<template>
+    <div :class="prefixCls">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+const prefixCls = 'qui-collapse'
+
+export default {
+    name: 'Collapse',
+    data() {
+        return {
+            prefixCls
+        }
+    },
+}
+</script>
+
+
+<style lang="scss" type="text/scss">
+    @import "../../styles/components/_collapse.scss";
+</style>
